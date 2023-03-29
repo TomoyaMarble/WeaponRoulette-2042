f983d704397dbc60ef13d66fb18609b976b4da3c
TomoyaMarble <68113544+TomoyaMarble@users.noreply.github.com>
2023-03-30 01:50:57 +0900

    add 4.1.0 weapons

--- roulette.js
+++ roulette.js
@@ -1,10 +1,8 @@
 function ramdom_up() {
-    const weapons_M = ["PBX-45", "PP-29", "MP9", "K30", "AC9", "M5A3", "AM40", "AK-24", "SFAR-M GL", "AC-42", "RM68", "LCMG", "アバンシーズ", "PKP-BP", "RPT-31", "DM7", "SVK", "VCAR", "BSV-M", "SWS-10", "DXR-1", "NTW-50", "MCS-880", "GVT 45-70", "12M オート", "ゴーストメーカーR10", "Rorsch Mk-4", "NVK-S22", "AKS-74U", "PP-2000", "M16A3", "ACW-R", "A-91", "M416", "MTAR-21", "M60E4", "XM8 LMG", "M240B", "Type 88 LMG", "M39 EMR", "SVD", "GOL スナイパーマグナム"];
     const weapons_M_2042 = ["PBX-45", "PP-29", "MP9", "K30", "AC9", "M5A3", "AM40", "AK-24", "SFAR-M GL", "AC-42", "RM68", "LCMG", "アバンシーズ", "PKP-BP", "RPT-31", "DM7", "SVK", "VCAR", "BSV-M", "SWS-10", "DXR-1", "NTW-50", "MCS-880", "GVT 45-70", "12M オート", "ゴーストメーカーR10", "Rorsch Mk-4", "NVK-S22"];
-    const weapons_M_vault = ["AKS-74U", "PP-2000", "M16A3", "ACW-R", "A-91", "M416", "MTAR-21", "M60E4", "XM8 LMG", "M240B", "Type 88 LMG", "M39 EMR", "SVD", "GOL スナイパーマグナム"];
-    const weapons_S = ["G57", "MP28", "M44", "PF51", "NVK-P125", "Super 500", "M1911", "MP412 REX", "M93R"];
+    const weapons_M_vault = ["AKS-74U", "PP-2000", "M16A3", "ACW-R", "A-91", "M416", "MTAR-21", "AEK-971", "M60E4", "XM8 LMG", "M240B", "Type 88 LMG", "M39 EMR", "RPK-74M", "SVD", "GOL スナイパーマグナム"];
     const weapons_S_2042 = ["G57", "MP28", "M44", "PF51", "NVK-P125", "Super 500"];
-    const weapons_S_vault = ["M1911", "MP412 REX", "M93R"];
+    const weapons_S_vault = ["M1911", "MP412 REX", "M93R", "MP443"];
     const gadgets_A = ["スモークランチャー", "IBA装甲板", "C5爆薬", "M18 クレイモア", "SPHエクスプローシブ・ランチャー"];
     const gadgets_E = ["対戦車地雷", "無反動M5", "FXM-33対空ミサイル", "C5爆薬", "FGM-148 ジャベリン", "EODボット"];
     const gadgets_S = ["弾薬箱", "医療箱", "スモークランチャー", "M18 クレイモア"];
@@ -15,6 +13,7 @@ function ramdom_up() {
     let random = 0;
     switch(sel.value){
         case "wm":
+            const weapons_M = weapons_M_2042.concat(weapons_M_vault);
             random = parseInt(Math.random() * weapons_M.length);
             document.getElementById('ywi').textContent = weapons_M[random];
             break;
@@ -27,6 +26,7 @@ function ramdom_up() {
             document.getElementById('ywi').textContent = weapons_M_vault[random];
             break;
         case "ws":
+            const weapons_S = weapons_S_2042.concat(weapons_S_vault);
             random = parseInt(Math.random() * weapons_S.length);
             document.getElementById('ywi').textContent = weapons_S[random];
             break;
