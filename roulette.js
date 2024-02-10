eb136e7d9b0f659bc782cb99a745bab9f467806d
TomoyaMarble <68113544+TomoyaMarble@users.noreply.github.com>
2024-02-10 20:32:22 +0900

    Update roulette.js

--- roulette.js
+++ roulette.js
@@ -1,12 +1,12 @@
 function ramdom_up() {
-    const weapons_M_2042 = ["PBX-45", "PP-29", "MP9", "K30", "AC9", "M5A3", "AM40", "AK-24", "SFAR-M GL", "AC-42", "RM68", "GEW-46", "LCMG", "アバンシーズ", "PKP-BP", "RPT-31", "DM7", "SVK", "VCAR", "BSV-M", "SWS-10", "DXR-1", "NTW-50", "XCE Bar", "MCS-880", "GVT 45-70", "12M オート", "ゴーストメーカーR10", "Rorsch Mk-4", "NVK-S22"];
+    const weapons_M_2042 = ["PBX-45", "PP-29", "MP9", "K30", "AC9", "M5A3", "AM40", "AK-24", "SFAR-M GL", "AC-42", "RM68", "GEW-46", "VHX-D3", "LCMG", "アバンシーズ", "PKP-BP", "RPT-31", "DM7", "SVK", "VCAR", "BSV-M", "G428", "SWS-10", "DXR-1", "NTW-50", "XCE Bar", "MCS-880", "GVT 45-70", "12M オート", "ゴーストメーカーR10", "Rorsch Mk-4", "NVK-S22"];
     const weapons_M_vault = ["AKS-74U", "PP-2000", "M16A3", "ACW-R", "A-91", "M416", "MTAR-21", "AEK-971", "M60E4", "XM8 LMG", "M240B", "Type 88 LMG", "M39 EMR", "RPK-74M", "SVD", "GOL スナイパーマグナム"];
-    const weapons_S_2042 = ["G57", "MP28", "M44", "PF51", "NVK-P125", "Super 500", "BFP.50"];
+    const weapons_S_2042 = ["G57", "MP28", "M44", "PF51", "NVK-P125", "Super 500", "L9CZ", "BFP.50"];
     const weapons_S_vault = ["M1911", "MP412 REX", "M93R", "MP443"];
     const gadgets_A = ["スモークランチャー", "IBA装甲板", "C5爆薬", "M18 クレイモア", "SPHエクスプローシブ・ランチャー"];
     const gadgets_E = ["対戦車地雷", "無反動M5", "FXM-33対空ミサイル", "C5爆薬", "FGM-148 ジャベリン", "EODボット", "RPG-7V2"];
-    const gadgets_S = ["弾薬箱", "医療箱", "スモークランチャー", "M18 クレイモア"];
-    const gadgets_R = ["T-UGS", "トレーサーダーツガン", "C5爆薬", "近接センサー"];
+    const gadgets_S = ["弾薬箱", "医療箱", "スモークランチャー", "M18 クレイモア" , "弾薬袋", "医療袋"];
+    const gadgets_R = ["T-UGS", "トレーサーダーツガン", "C5爆薬", "M18 クレイモア", "近接センサー", "SOFLAM"];
     const throwable = ["破片手榴弾", "発煙弾", "EMPグレネード", "焼夷グレネード", "コンカッショングレネード", "スローイングナイフ", "対戦車爆弾", "小型手榴弾", "スプリンググレネード"];
     const text = document.getElementById("ywi");
     const sel = document.getElementById("mode-select");
